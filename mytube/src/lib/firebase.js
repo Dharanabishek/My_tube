a67e1edd945c3ee6cc365111e7da197eb8d8c769
Dharanabishek <dharanabi2005@gmail.com>
2026-06-13 22:42:14 +0530

    Update firebase config

--- mytube/src/lib/firebase.js
+++ mytube/src/lib/firebase.js
@@ -6,14 +6,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
-  authDomain: "yourtube-8cda9.firebaseapp.com",
-  projectId: "yourtube-8cda9",
-  storageBucket: "yourtube-8cda9.firebasestorage.app",
-  messagingSenderId: "921641878423",
-  appId: "1:921641878423:web:0d65801eebaf2b25f03ad2",
+  apiKey: "AIzaSyCNDO_we2VK30NFIB-GrD6I6buaoiq-1mw",
+  authDomain: "my-tube-app-9f040.firebaseapp.com",
+  projectId: "my-tube-app-9f040",
+  storageBucket: "my-tube-app-9f040.firebasestorage.app",
+  messagingSenderId: "48291323929",
+  appId: "1:48291323929:web:e3a0b23bbb901393040a88",
+  measurementId: "G-N34WLMCW31"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
